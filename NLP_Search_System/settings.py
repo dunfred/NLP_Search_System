2f69798b2a51aa7c05e2abd0e495f163e6423b6c
Dunyo Fred <donklenam2@gmail.com>
2020-01-24 19:45:00 +0000

    Update settings.py

--- NLP_Search_System/settings.py
+++ NLP_Search_System/settings.py
@@ -122,7 +122,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
