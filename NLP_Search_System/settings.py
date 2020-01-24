605568760d6ef9a1db85cc27d3ab673cd79f786c
Dunyo Fred <donklenam2@gmail.com>
2020-01-24 18:33:19 +0000

    Update settings.py

--- NLP_Search_System/settings.py
+++ NLP_Search_System/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -110,11 +111,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/search_engine/static/'
+STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, '/search_engine/static'),
+        os.path.join(PROJECT_ROOT, 'static'),
     )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -126,3 +127,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
